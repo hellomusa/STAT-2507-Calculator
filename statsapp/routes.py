511c63c01960157e25704d85e703cc6841c89563
helloMusa <esquitoban@gmail.com>
2019-11-11 14:38:53 -0500

    Adding pages (mean, median) wip

--- statsapp/routes.py
+++ statsapp/routes.py
@@ -4,11 +4,17 @@ from statsapp import app
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return render_template('index.html', title='about')
+    return render_template('layout.html', title='about')
 
 @app.route('/mean')
 def mean():
+	return render_template('mean.html', title='mean')
+
+
+@app.route('/median')
+def median():
+	return render_template('median.html', title='median')
