243ecd291c2e45d03ae6cbd89e47f7dc38fa7de1
helloMusa <esquitoban@gmail.com>
2019-11-12 18:55:54 -0500

    Added functionality for more calculations

--- statsapp/funcs.py
+++ statsapp/funcs.py
@@ -4,26 +4,21 @@ import numpy as np
 
 # Mean
 def get_mean(data):
-	data_sum = 0
-	size = 0
-	for n in data:
-		data_sum += n
-		size += 1
-	mean = data_sum / size
+	mean = sum(data) / len(data)
 	return mean
 
 
 # Median
-def get_median():
+def get_median(data):
 	data.sort()
-	median_rank = (size + 1)/2
+	median_rank = (len(data) + 1)/2
 	median_val = statistics.median(data)
 	median = [median_rank, median_val] #rank at m[0], val at m[1]
 	return median
 
 
 # Mode
-def get_mode():
+def get_mode(data):
 	mode = statistics.mode(data)
 	return mode
 
@@ -40,32 +35,32 @@ def get_min():
 
 
 # Population Variance:
-def get_pop_variance():
-	pop_variance = statistics.pvariance(data, get_mean())
+def get_pop_variance(data):
+	pop_variance = statistics.pvariance(data, get_mean(data))
 	return pop_variance
 
 
 # Sample Variance:
-def get_sam_variance():
-	sam_variance = statistics.variance(data, get_mean())
+def get_sam_variance(data):
+	sam_variance = statistics.variance(data, get_mean(data))
 	return sam_variance
 
 
 # Population Standard Deviation
-def get_pop_sd():
+def get_pop_sd(data):
 	pop_sd = statistics.pstdev(data)
 	return pop_sd
 
 
 # Sample Standard Deviation
-def get_sam_sd():
+def get_sam_sd(data):
 	sam_sd = statistics.stdev(data)
 	return sam_sd
 
 
 # Sample z-score
-def get_zscore(x):
-	z_score = ((x - get_mean()) / get_sam_sd())
+def get_zscore(data, x):
+	z_score = ((x - get_mean(data)) / get_sam_sd(data))
 	return z_score
 
 
--- statsapp/routes.py
+++ statsapp/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from statsapp import app, db
-from statsapp.forms import DataForm
+from statsapp.forms import DataForm, ArgForm
 from statsapp.models import Data
 from statsapp.funcs import *
 
@@ -34,71 +34,116 @@ def about():
 def mean():
 	form = DataForm()
 
-	# If valid data entered
 	if form.validate_on_submit():
 		data = [float(i) for i in form.body.data.split(',')]
-		print(data)
 		result = round(get_mean(data), 2)
 		return render_template('mean.html', title='mean', form=form, operation='Mean', result=result)
 
 	return render_template('mean.html', title='mean', form=form, operation='Mean')
 
 
-@app.route('/median')
+@app.route('/median', methods=['GET', 'POST'])
 def median():
-	return render_template('median.html', title='median')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		result = get_median([float(i) for i in form.body.data.split(',')])
+		return render_template('median.html', title='median', form=form, operation='Median', result=result)
+
+	return render_template('median.html', title='median', form=form, operation='Median')
 
 
-@app.route('/mode')
+@app.route('/mode', methods=['GET', 'POST'])
 def mode():
-	return render_template('mode.html', title='mode')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		try:
+			result = get_mode([float(i) for i in form.body.data.split(',')])
+			return render_template('mode.html', title='mode', form=form, operation='Mode', result=result)
+
+		except statistics.StatisticsError:
+			return render_template('mode.html', title='mode', form=form, operation='Mode', result='No unique mode')
+
+	return render_template('mode.html', title='mode', form=form, operation='Mode')
 
 
-@app.route('/samplevariance')
+@app.route('/samplevariance', methods=['GET', 'POST'])
 def samvar():
-	return render_template('samvar.html', title='sample variance')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		result = get_sam_variance([float(i) for i in form.body.data.split(',')])
+		return render_template('samvar.html', title='sample variance', form=form, operation='Sample Variance', result=result)
+
+	return render_template('samvar.html', title='sample variance', form=form, operation='Sample Variance')
 
 
-@app.route('/populationvariance')
+@app.route('/populationvariance', methods=['GET', 'POST'])
 def popvar():
-	return render_template('popvar.html', title='population variance')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		result = get_pop_variance([float(i) for i in form.body.data.split(',')])
+		return render_template('popvar.html', title='population variance', form=form, operation='Population Variance', result=result)
+
+	return render_template('popvar.html', title='population variance', form=form, operation='Population Variance')
 
 
-@app.route('/samplesd')
+@app.route('/samplesd', methods=['GET', 'POST'])
 def samsd():
-	return render_template('samsd.html', title='sample standard deviation')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		result = get_sam_sd([float(i) for i in form.body.data.split(',')])
+		return render_template('samsd.html', title='sample standard deviation', form=form, operation='Sample Standard Deviation', result=result)
+
+	return render_template('samsd.html', title='sample standard deviation', form=form, operation='Sample Standard Deviation')
 
 
-@app.route('/populationsd')
+@app.route('/populationsd', methods=['GET', 'POST'])
 def popsd():
-	return render_template('popsd.html', title='population standard deviation')
+	form = DataForm()
+
+	if form.validate_on_submit():
+		result = get_pop_sd([float(i) for i in form.body.data.split(',')])
+		return render_template('popsd.html', title='population standard deviation', form=form, operation='Population Standard Deviation', result=result)
+
+	return render_template('popsd.html', title='population standard deviation', form=form, operation='Population Standard Deviation')
 
 
-@app.route('/zscore')
+@app.route('/zscore', methods=['GET', 'POST'])
 def zscore():
-	return render_template('zscore.html', title='z-score')
+	form = DataForm()
+	x_form = ArgForm()
+
+	if x_form.validate_on_submit():
+		result = get_zscore([float(i) for i in form.body.data.split(',')], x_form.body.data)
+		return render_template('zscore.html', title='zscore', form=form, argform=x_form, operation='Z-Score Calculator', result=result)
+
+	return render_template('zscore.html', title='zscore', form=form, argform=x_form, operation='Z-Score Calculator')
 
 
-@app.route('/percentile')
+@app.route('/percentile', methods=['GET', 'POST'])
 def percentile():
 	return render_template('percentile.html', title='percentile')
 
 
-@app.route('/iqr')
+@app.route('/iqr', methods=['GET', 'POST'])
 def iqr():
 	return render_template('iqr.html', title='iqr')
 
 
-@app.route('/fivenumsumm')
+@app.route('/fivenumsumm', methods=['GET', 'POST'])
 def fivenumsumm():
 	return render_template('fivenum.html', title='five number summary')
 
 
-@app.route('/combination')
+@app.route('/combination', methods=['GET', 'POST'])
 def combination():
 	return render_template('combination.html', title='combination')
 
 
-@app.route('/permutation')
+@app.route('/permutation', methods=['GET', 'POST'])
 def permutation():
 	return render_template('permutation.html', title='permutation')
